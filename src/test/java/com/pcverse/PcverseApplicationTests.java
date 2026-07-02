67d9d3f5e21cde470fbcbe639a80788711fecb53
hoanganh <hoanganh275275@gmail.com>
2026-07-03 00:12:54 +0700

    initialize project structure with Spring Boot and PostgreSQL setup

---
+++ src/test/java/com/pcverse/PcverseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pcverse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcverseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
