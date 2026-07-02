a09d707d25608eb703cb49cf6600b5f942280215
hoanganh <hoanganh275275@gmail.com>
2026-07-03 01:54:45 +0700

    add AbstractAuditingEntity class for auditing support in entities

---
+++ src/main/java/com/pcverse/entity/AbstractAuditingEntity.java
@@ -0,0 +1,48 @@
+package com.pcverse.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@JsonIgnoreProperties(
+        value = {"createdBy", "createdDate", "lastModifiedBy", "lastModifiedDate"},
+        allowGetters = true
+)
+public class AbstractAuditingEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_at")
+    private Instant lastModifiedAt;
+
+}
