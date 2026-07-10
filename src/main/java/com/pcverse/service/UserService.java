b6e3a868eb7f2e5a7bb8af56cca0d27eece1a414
hoanganh <hoanganh275275@gmail.com>
2026-07-10 19:22:32 +0700

    add: implement getAllUsers method in UserService and UserController

--- src/main/java/com/pcverse/service/UserService.java
+++ src/main/java/com/pcverse/service/UserService.java
@@ -4,10 +4,14 @@ import com.pcverse.dto.request.CreateUserRequest;
 import com.pcverse.dto.response.CreateUserResponse;
 import com.pcverse.dto.response.UserDetailsResponse;
 
+import java.util.List;
+
 public interface UserService {
 
     CreateUserResponse createUser(CreateUserRequest request);
 
     UserDetailsResponse myInfo(String userId);
+
+    List<UserDetailsResponse> getAllUsers();
 
 }
